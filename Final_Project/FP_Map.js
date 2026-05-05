46dade4ab39e5989647ba567c17c116a51c32685
Jack Wheeler <wheelerwilliam548@gmail.com>
2026-05-04 22:14:10 -0500

    Add files via upload

--- Final_Project/FP_Map.js
+++ Final_Project/FP_Map.js
@@ -1,14 +1,14 @@
-// INITIALIZE MAP
+// === INITIALIZE MAP === //
 const mymap = L.map('map', {
   center: [35.5, -79],
   zoom: 7
 });
 
-// BASE LAYERS
+// === BASE LAYERS === //
 const CartoDB_DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-	subdomains: 'abcd',
-	maxZoom: 20
+  attribution: '&copy; OpenStreetMap contributors &copy; CARTO',
+  subdomains: 'abcd',
+  maxZoom: 20
 }).addTo(mymap);
 
 const Esri_WorldImagery = L.tileLayer(
@@ -16,7 +16,7 @@ const Esri_WorldImagery = L.tileLayer(
   { attribution: 'Tiles © Esri' }
 );
 
-// MINIMAP
+// === MINIMAP === //
 const miniLayer = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
 
 new L.Control.MiniMap(miniLayer, {
@@ -24,34 +24,29 @@ new L.Control.MiniMap(miniLayer, {
   minimized: false
 }).addTo(mymap);
 
-// RESET VIEW
+// === RESET VIEW === //
 const fullExtent = [35.5, -79];
 const fullZoom = 7;
 
 L.easyButton(
-  '<img src="FPimages/Globe_icon.png" height="20px">',
+  '<img src="./FPImages/globe_dark.svg" height="50%">',
   function () {
     mymap.flyTo(fullExtent, fullZoom, { duration: 2 });
   },
   "Return to world view"
 ).addTo(mymap);
 
-// COLOR FUNCTIONS
-// OVERLAY #1: USPVDB (Cool Palette)
+// === COLOR FUNCTIONS === //
 function getCapacityColor(mw) {
   if (!mw) return "#ccc";
 
   return mw > 80 ? '#084081' :
-         mw > 60 ? '#0868ac' :
          mw > 40 ? '#2b8cbe' :
          mw > 20 ? '#4eb3d3' :
-         mw > 10 ? '#7bccc4' :
          mw > 5  ? '#a8ddb5' :
-         mw > 1  ? '#ccebc5' :
-                   '#f0f9e8';
+                    '#f0f9e8';
 }
 
-// OVERLAY #2: DNI (Warm Palette)
 function getDNIColor(value) {
   if (!value) return "#ccc";
 
@@ -65,12 +60,34 @@ function getDNIColor(value) {
                          '#fff5eb';
 }
 
-// CONTEXT LAYER: NC STATE BOUNDARY
+function getAspectColor(val) {
+  const num = Number(val);
+  if (isNaN(num)) return "#ccc";
+
+  if (num === 1) return "#d7191c";
+  if (num === 2) return "#fdae61";
+  if (num === 3) return "#1a9641";
+
+  return "#ccc";
+}
+
+function getSlopeColor(val) {
+  const num = Number(val);
+  if (isNaN(num)) return "#ccc";
+
+  if (num === 1) return "#1a9641";
+  if (num === 2) return "#fdae61";
+  if (num === 3) return "#d7191c";
+
+  return "#ccc";
+}
+
+// === CONTEXT LAYER: NC STATE BOUNDARY === //
 const ncBoundary = L.geoJSON(null, {
   style: function () {
     return {
       color: "#ffffff",
-      weight: 2,
+      weight: 1,
       opacity: 1,
       fill: false
     };
@@ -81,7 +98,7 @@ const ncBoundaryHalo = L.geoJSON(null, {
   style: function () {
     return {
       color: "#d3d3d3",
-      weight: 3,
+      weight: 2,
       opacity: 0.6,
       fill: false
     };
@@ -95,20 +112,19 @@ fetch("./Data/NC_Bound.json")
     ncBoundary.addData(data);
   });
 
-// OVERLAY #1: EXISTING PROJECTS LAYER
-const currentProjects = L.geoJSON(null, {
-
-  style: function (feature) {
+// === OVERLAY #1: CAPACITY LAYER === //
+const capacityPoints = L.geoJSON(null, {
+  pointToLayer: function (feature, latlng) {
     const color = getCapacityColor(feature.properties.p_cap_ac);
 
-    return {
-      color: color,
-      weight: 3,
-      fillOpacity: 0.9,
-      fillColor: color
-    };
+    return L.circleMarker(latlng, {
+      radius: 4,
+      color: "#ffffff",
+      weight: 1.0,
+      fillColor: color,
+      fillOpacity: 0.9
+    });
   },
-
   onEachFeature: function (feature, layer) {
     const p = feature.properties;
 
@@ -121,28 +137,27 @@ const currentProjects = L.geoJSON(null, {
           <b>Longitude:</b> ${p.xlong || "N/A"}<br>
           <b>Axis Type:</b> ${p.p_axis || "N/A"}<br>
           <b>Azimuth:</b> ${p.p_azimuth || "N/A"}<br>
+          <b>Land Cover:</b> ${p.p_agrivolt || "N/A"}<br>
+          <b>Type:</b> ${p.p_type || "N/A"}<br>
+          <hr>
           <b>Capacity (AC):</b> ${p.p_cap_ac || "N/A"} MW<br>
           <b>Capacity (DC):</b> ${p.p_cap_dc || "N/A"} MW<br>
-          <b>Land Cover:</b> ${p.p_agrivolt || "N/A"}<br>
-          <b>Type:</b> ${p.p_type || "N/A"}
         </div>
       `;
-
       layer.bindPopup(popupContent);
     }
   }
 }).addTo(mymap);
 
-fetch("./Data/uspvdb_NC.geojson")
+fetch("./Data/uspvdb_Point.json")
   .then(res => res.json())
   .then(data => {
-    currentProjects.addData(data);
-    mymap.fitBounds(currentProjects.getBounds());
+    capacityPoints.addData(data);
+    capacityPoints.bringToFront();
   });
 
-// OVERLAY #2: NC DNI LAYER
+// === OVERLAY #2: NC DNI LAYER === //
 const ncDNI = L.geoJSON(null, {
-
   style: function (feature) {
     const val = feature.properties.ANNUAL_AVG;
 
@@ -153,7 +168,6 @@ const ncDNI = L.geoJSON(null, {
       fillColor: getDNIColor(val)
     };
   },
-
   onEachFeature: function (feature, layer) {
     const val = feature.properties.ANNUAL_AVG;
 
@@ -163,7 +177,7 @@ const ncDNI = L.geoJSON(null, {
       </div>
     `);
   }
-}).addTo(mymap);
+});
 
 fetch("./Data/NC_DNI.json")
   .then(res => res.json())
@@ -171,86 +185,173 @@ fetch("./Data/NC_DNI.json")
     ncDNI.addData(data);
   });
 
-// LAYER CONTROL
+// === OVERLAY #3: ASPECT LAYER === //
+const ncAspect = L.geoJSON(null, {
+  style: function (feature) {
+    const val = feature.id;
+    return {
+      color: "#333",
+      weight: 1,
+      fillOpacity: 0.75,
+      fillColor: getAspectColor(val)
+    };
+  }
+});
+
+fetch("./Data/NC_Aspect.geojson")
+  .then(res => res.json())
+  .then(data => {
+    ncAspect.addData(data);
+
+    if (ncAspect.getBounds().isValid()) {
+      map.fitBounds(ncAspect.getBounds());
+    }
+  });
+
+// === OVERLAY #4: SLOPE LAYER === //
+const ncSlope = L.geoJSON(null, {
+  style: function (feature) {
+    const val = feature.id;
+    return {
+      color: "#333",
+      weight: 1,
+      fillOpacity: 0.75,
+      fillColor: getSlopeColor(val)
+    };
+  }
+});
+
+fetch("./Data/NC_Slope.geojson")
+  .then(res => res.json())
+  .then(data => {
+    ncSlope.addData(data);
+
+    if (ncSlope.getBounds().isValid()) {
+      map.fitBounds(ncSlope.getBounds());
+    }
+  });
+
+// === LAYER CONTROL === //
 const baseMaps = {
   "Dark": CartoDB_DarkMatter,
   "Imagery": Esri_WorldImagery
 };
 
 const overlays = {
-  "Existing Projects": currentProjects,
-  "Solar Resource (DNI)": ncDNI
+  "Existing Projects": capacityPoints,
+  "Solar Resource (DNI)": ncDNI,
+  "Aspect": ncAspect,
+  "Slope": ncSlope
 };
 
 L.control.layers(baseMaps, overlays, { collapsed: false }).addTo(mymap);
 
-
-// LEGEND FUNCTIONS
-// OVERLAY #1: CAPACITY LEGEND
+// === LEGENDS === //
 const capacityLegend = L.control({ position: "bottomright" });
 
 capacityLegend.onAdd = function () {
   const div = L.DomUtil.create("div", "info legend");
-  const grades = [1, 5, 10, 20, 40, 60, 80];
-
   div.innerHTML += "<h4>Capacity (MW)</h4>";
 
-  for (let i = 0; i < grades.length; i++) {
+  const breaks = [[0,5],[5,20],[20,40],[40,80]];
+
+  breaks.forEach(b => {
     div.innerHTML +=
-      '<i style="background:' + getCapacityColor(grades[i] + 1) +
+      '<i style="background:' + getCapacityColor(b[1]-0.01) +
       '; width:18px; height:18px; display:inline-block; margin-right:8px;"></i> ' +
-      grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-  }
+      b[0] + '&ndash;' + b[1] + '<br>';
+  });
+
+  div.innerHTML +=
+    '<i style="background:' + getCapacityColor(81) +
+    '; width:18px; height:18px; display:inline-block; margin-right:8px;"></i> 80+';
 
   return div;
 };
 
-// OVERLAY #2: DNI LEGEND
 const dniLegend = L.control({ position: "bottomright" });
 
 dniLegend.onAdd = function () {
   const div = L.DomUtil.create("div", "info legend");
 
-  const grades = [3600, 3800, 4000, 4200, 4400, 4600, 4800];
+  const grades = [3600,3800,4000,4200,4400,4600,4800];
 
   div.innerHTML += "<h4>Annual Avg DNI</h4>";
 
-  for (let i = 0; i < grades.length; i++) {
+  for (let i=0;i<grades.length;i++){
     div.innerHTML +=
-      '<i style="background:' + getDNIColor(grades[i] + 1) +
+      '<i style="background:' + getDNIColor(grades[i]+1) +
       '; width:18px; height:18px; display:inline-block; margin-right:8px;"></i> ' +
-      grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+      grades[i] + (grades[i+1] ? '&ndash;'+grades[i+1]+'<br>' : '+');
   }
 
   return div;
 };
 
-// DEFAULT LEGEND
+// === ASPECT LEGEND === //
+const aspectLegend = L.control({ position: "bottomright" });
+
+aspectLegend.onAdd = function () {
+  const div = L.DomUtil.create("div", "info legend");
+
+  div.innerHTML += "<h4>Aspect</h4>";
+
+  div.innerHTML +=
+    '<i style="background:#1a9641; width:18px; height:18px; display:inline-block; margin-right:8px;"></i> Ideal<br>' +
+    '<i style="background:#fdae61; width:18px; height:18px; display:inline-block; margin-right:8px;"></i> Acceptable<br>' +
+    '<i style="background:#d7191c; width:18px; height:18px; display:inline-block; margin-right:8px;"></i> Avoid';
+
+  return div;
+};
+
+// === SLOPE LEGEND === //
+const slopeLegend = L.control({ position: "bottomright" });
+
+slopeLegend.onAdd = function () {
+  const div = L.DomUtil.create("div", "info legend");
+
+  div.innerHTML += "<h4>Slope</h4>";
+
+  div.innerHTML +=
+    '<i style="background:#1a9641; width:18px; height:18px; display:inline-block; margin-right:8px;"></i> Ideal<br>' +
+    '<i style="background:#fdae61; width:18px; height:18px; display:inline-block; margin-right:8px;"></i> Acceptable<br>' +
+    '<i style="background:#d7191c; width:18px; height:18px; display:inline-block; margin-right:8px;"></i> Avoid';
+
+  return div;
+};
+
+// DEFAULT LEGEND ON LOAD
 capacityLegend.addTo(mymap);
 
-// MUTUAL EXCLUSION LOGIC <--- Needs work, does not function properly
+// === LEGEND CONTROL === //
+function clearLegends() {
+  mymap.removeControl(capacityLegend);
+  mymap.removeControl(dniLegend);
+  mymap.removeControl(aspectLegend);
+  mymap.removeControl(slopeLegend);
+}
+
+function removeOtherLayers(activeLayer) {
+  const layers = [capacityPoints, ncDNI, ncAspect, ncSlope];
+
+  layers.forEach(layer => {
+    if (layer !== activeLayer && mymap.hasLayer(layer)) {
+      mymap.removeLayer(layer);
+    }
+  });
+}
+
 mymap.on('overlayadd', function (e) {
 
-  if (e.layer === currentProjects) {
-    mymap.removeLayer(ncDNI);
-    mymap.removeControl(dniLegend);
-    capacityLegend.addTo(mymap);
-  }
+  removeOtherLayers(e.layer);
+  clearLegends();
 
-  if (e.layer === ncDNI) {
-    mymap.removeLayer(currentProjects);
-    mymap.removeControl(capacityLegend);
-    dniLegend.addTo(mymap);
-  }
+  if (e.layer === capacityPoints) capacityLegend.addTo(mymap);
+  if (e.layer === ncDNI) dniLegend.addTo(mymap);
+  if (e.layer === ncAspect) aspectLegend.addTo(mymap);
+  if (e.layer === ncSlope) slopeLegend.addTo(mymap);
 });
 
-mymap.on('overlayremove', function (e) {
-
-  if (e.layer === currentProjects) {
-    mymap.removeControl(capacityLegend);
-  }
-
-  if (e.layer === ncDNI) {
-    mymap.removeControl(dniLegend);
-  }
+mymap.on('overlayremove', function () {
+  clearLegends();
 });
